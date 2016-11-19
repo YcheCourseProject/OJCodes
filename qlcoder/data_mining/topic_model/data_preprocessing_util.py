bb62657324b713c3ce60919ca465ea33794fe7b7
cheyulin <yche@cse.ust.hk>
2016-11-19 22:34:12 +0800

    hard to continue topic model with lda

---
+++ qlcoder/data_mining/topic_model/data_preprocessing_util.py
@@ -0,0 +1,94 @@
+# ~/anaconda2/bin/python
+# coding:utf-8
+
+import sys
+import os
+import re
+import jieba.analyse
+
+reload(sys)
+sys.setdefaultencoding("utf8")
+
+stop_words_set = {}
+stop_file_name = 'cn_stop_words.txt'
+res_output_file_name = 'output_whole_corpus.txt'
+bad_info_file_name = 'output_bad_info.txt'
+
+
+def init_stop_words_set(file_name):
+    global stop_words_set
+    with open(file_name, 'r') as file:
+        stop_words_set = {' '} | set([line.strip() for line in file])
+
+
+def generate_whole_corpus_file():
+    with open(res_output_file_name, 'w') as ofs:
+        walk = os.walk('./8000')
+        line_list = {}
+        for root, dir, files in walk:
+            for file_name in files:
+                my_path = root + '/' + file_name
+                line_list[file_name.rstrip('.txt')] = extract_key_words(my_path)
+        ofs.write(str(line_list))
+
+
+def is_filtered_pattern(your_str):
+    your_str = str(your_str)
+    if re.match('[0-9]+', your_str):
+        return True
+    if re.match('[a-zA-Z]+', your_str):
+        return True
+    if your_str in stop_words_set:
+        # print your_str
+        return True
+
+
+def extract_key_words(input_file_name):
+    with open(input_file_name, 'r') as ifs:
+        lines = ifs.readlines()
+        line = '\n'.join(lines)
+        key_words = jieba.analyse.extract_tags(line, topK=20)
+        # print key_words
+        key_words = filter(lambda ele: not is_filtered_pattern(ele), key_words)
+        return key_words
+
+
+def read_file_get_dict():
+    with open(res_output_file_name) as ifs:
+        eval_str = ifs.readline()
+        my_dict = eval(eval_str)
+        return my_dict
+
+
+def find_bad_things():
+    my_dict = read_file_get_dict()
+    bad_list = []
+    with open('output_readable.txt', 'w') as readable_ofs:
+        with open(bad_info_file_name, 'w') as ofs:
+            for i in range(8000):
+                if len(my_dict[str(i)]) < 10:
+                    tmp_str = 'question:' + str(i) + ' data:' + ','.join(my_dict[str(i)]) + 'data length:' + str(
+                        len(my_dict[str(i)])) + '\n'
+                    bad_list.append(str(i))
+                    ofs.write(tmp_str)
+                else:
+                    readable_ofs.write('question:' + str(i) + ' data:' + ','.join(my_dict[str(i)]) + '\n')
+
+    return set(bad_list)
+
+
+def return_useful_words_list():
+    my_dict = read_file_get_dict()
+    bad_set = find_bad_things()
+    words_list = []
+    for ele in my_dict:
+        if ele not in bad_set:
+            words_list.append(my_dict[ele])
+    return words_list
+
+
+if __name__ == '__main__':
+    init_stop_words_set(stop_file_name)
+    generate_whole_corpus_file()
+    filtered_word_list = return_useful_words_list()
+    print len(filtered_word_list)
--- qlcoder/data_mining/topic_model/topic_model.py
+++ qlcoder/data_mining/topic_model/topic_model.py
@@ -2,50 +2,22 @@
 # coding:utf-8
 
 import sys
-import os
-import jieba
 from gensim import corpora, models
+import data_preprocessing_util
 
 reload(sys)
 sys.setdefaultencoding("utf8")
 
-
-def get_stop_words_set(file_name):
-    with open(file_name, 'r') as file:
-        return {' '} | set([line.strip() for line in file])
-
-
-def get_words_list(file_name, stop_word_file):
-    stop_words_set = get_stop_words_set(stop_word_file)
-    print "共计导入 %d 个停用词" % len(stop_words_set)
-    word_list = []
-    with open(file_name, 'r') as file:
-        for line in file:
-            tmp_list = list(jieba.cut(line.strip(), cut_all=False))
-            # 注意这里term是unicode类型，如果不转成str，判断会为假
-            word_list.append([term for term in tmp_list if str(term) not in stop_words_set])
-    return word_list
-
-
 if __name__ == '__main__':
-    raw_msg_file = '8000/1.txt'
-    stop_word_file = 'cn_stop_words.txt'
-
     # 列表，其中每个元素也是一个列表，即每行文字分词后形成的词语列表
-    word_list = get_words_list(raw_msg_file, stop_word_file)
-    print word_list
+    word_list = data_preprocessing_util.return_useful_words_list()
 
     # 生成文档的词典，每个词与一个整型索引值对应
     word_dict = corpora.Dictionary(word_list)
-    print word_dict
 
     # 词频统计，转化成空间向量格式
     corpus_list = [word_dict.doc2bow(text) for text in word_list]
-    print corpus_list
     lda = models.ldamodel.LdaModel(corpus=corpus_list, id2word=word_dict, num_topics=8, alpha='auto')
 
-    output_file = './output_lda.txt'
-    with open(output_file, 'w') as f:
-        for pattern in lda.show_topics():
-            print >> f, "%s" % str(pattern)
-            print pattern[1]
+    for pattern in lda.show_topics():
+        print 'topic', pattern[0], str(pattern[1])
