11de8fd09f3c89175d32e311fce96714fb93a0b2
cheyulin <yche@cse.ust.hk>
2016-11-24 23:34:36 +0800

    finish it

--- qlcoder/image_processing/image_similarity/image_hash.py
+++ qlcoder/image_processing/image_similarity/image_hash.py
@@ -9,10 +9,10 @@ def is_image(filename):
            f.endswith(".jpeg") or f.endswith(".bmp") or f.endswith(".gif")
 
 
-def compute_image2_hash_vals():
+def compute_image_hash_vals(path_info):
     ret_dict = {}
     dir_path = os.path.join(os.path.dirname(__file__))
-    walk_image2 = os.walk(dir_path + '/../datasets/image2')
+    walk_image2 = os.walk(dir_path + '/../datasets/' + path_info)
     for root, dir, files in walk_image2:
         for file_name in files:
             if file_name.endswith('jpg') or file_name.endswith('png'):
@@ -29,6 +29,20 @@ def compute_similar_bit_in_bool_arr(left, right):
     return count
 
 
-image2_hash_dict = compute_image2_hash_vals()
+image2_hash_dict = compute_image_hash_vals('image2')
+image1_hash_dict = compute_image_hash_vals('image1')
+image2_res_list = []
 for i in range(len(image2_hash_dict)):
-    print image2_hash_dict[i + 1]
+    tmp = image2_hash_dict[i + 1]
+    tmp_idx = --1
+    min = 99999
+    for ele in image1_hash_dict:
+        if abs(tmp - image1_hash_dict[ele]) < min:
+            tmp_idx = ele
+            min = abs(tmp - image1_hash_dict[ele])
+    image2_res_list.append(tmp_idx)
+
+print ','.join(map(str,image2_res_list))
+print reduce(lambda left, right: left + right, image2_res_list)
+# for i in range(len(image2_hash_dict)):
+# print image2_hash_dict[i + 1]
