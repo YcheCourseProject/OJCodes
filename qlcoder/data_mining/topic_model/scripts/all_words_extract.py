a673b27d0ebb51e906baf11a1fab8ba7aeec61bd
cheyulin <yche@cse.ust.hk>
2016-12-02 22:30:40 +0800

    update

--- qlcoder/data_mining/topic_model/scripts/all_words_extract.py
+++ qlcoder/data_mining/topic_model/scripts/all_words_extract.py
@@ -10,14 +10,18 @@ def is_filtered_pattern(your_str):
         return True
 
 
-with open('../output_all.txt') as ifs:
-    lines = ifs.readlines()
-    lines = map(lambda line: line.lstrip().rstrip(), lines)
-    all_words = []
-    for line in lines:
-        all_words.extend(filter(lambda ele: len(ele) > 0, re.split(r'\s|&.*|;',line)))
-    print len(all_words)
-    all_words.sort()
-    all_words = set(all_words)
-    with open('../output_all_sorted.txt', 'w') as ofs:
-        ofs.write('\n'.join(all_words))
+def get_sorted_lines():
+    with open('../output_all.txt') as ifs:
+        lines = ifs.readlines()
+        lines = map(lambda line: line.lstrip().rstrip(), lines)
+        all_words = []
+        for line in lines:
+            all_words.extend(re.sub(r'\xe3\x80\x80|&nbsp;|&nbsp|&gt;|&gt|\x00', '', line).split())
+        print len(all_words)
+        all_words.sort()
+        all_words = set(all_words)
+        with open('../output_all_sorted.txt', 'w') as ofs:
+            ofs.write('\n'.join(all_words))
+
+
+get_sorted_lines()
--- qlcoder/data_mining/topic_model/scripts/beauty_files.py
+++ qlcoder/data_mining/topic_model/scripts/beauty_files.py
@@ -17,20 +17,38 @@ def is_bad_pattern(line_str):
     line_str = line_str.rstrip().lstrip()
     if len(line_str) == 0:
         return True
+        # else:
+        # if re.match('badcase.*', line_str):
+        #     bad_case_set.add(line_str.split()[1])
+        # return True
+
+        # return False
+
+
+def extract_link(line):
+    if re.match(r'.*\[.*\]', line):
+        print 'match'
+        links = re.findall(r'\[.*\]', line)
+        return links
     else:
-        if re.match('badcase.*', line_str):
-            bad_case_set.add(line_str.split()[1])
-            return True
+        return None
 
-        return False
-
-
-with open('../8000/1.txt') as ifs:
+with open('../8000/5.txt') as ifs:
     lines = ifs.readlines()
-    lines = map(lambda ele: ele.rstrip().lstrip(), lines)
-    lines = filter(lambda ele: not is_bad_pattern(ele), lines)
+    new_lines = []
     for line in lines:
+        new_lines.extend(re.sub(r'\xe3\x80\x80|&nbsp;|&nbsp|&gt;|&gt|\x00', '', line).split())
+
+    new_lines = map(lambda ele: ele.rstrip().lstrip(), new_lines)
+    new_lines = filter(lambda ele: not is_bad_pattern(ele), new_lines)
+    new_lines = map(lambda ele: ele.lstrip().rstrip(), new_lines)
+
+    for line in new_lines:
         print 'line:', line
+        links = extract_link(line)
+        if not links  is None:
+            print links
+    print new_lines
 
 for ele in bad_case_set:
     print bad_case_set
