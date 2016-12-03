6bf9d729a4ecb8040c4be1d55e2c756f1ac99222
cheyulin <yche@cse.ust.hk>
2016-12-03 11:24:55 +0800

    remove blank

--- qlcoder/data_mining/topic_model/scripts/all_words_extract.py
+++ qlcoder/data_mining/topic_model/scripts/all_words_extract.py
@@ -3,27 +3,41 @@
 
 import re
 
+all_eng_count = 0
+
 
 def get_lines_single_file(file_name):
     with open(file_name) as ifs:
         lines = ifs.readlines()
         new_lines = []
         for line in lines:
-            new_lines.extend(re.sub(r'\xe3\x80\x80|&nbsp;|&nbsp|&gt;|&gt|\x00', '\n', line).split())
+            new_lines.extend(re.sub(r'\xe3\x80\x80|&nbsp;|&nbsp|&gt;|&gt|\x00|\s', '', line).split())
 
         ret_lines = []
+        eng_words_set = set()
 
         is_bad = False
+
         for i in range(len(new_lines)):
             line = new_lines[i]
             if is_bad:
                 is_bad = False
                 continue
-            if re.match('.*可能与主题无关的词.*', line) or re.match('.*badcase.*', line) or re.match('.*噪音词.*', line):
+            elif re.match('.*可能与主题无关的词.*', line) or re.match('.*badcase.*', line) or re.match('.*噪音词.*', line):
                 is_bad = True
                 continue
             ret_lines.append(line)
 
+        pattern_eng = re.compile('.*[a-zA-Z]+.*')
+        for line in ret_lines:
+            if re.match(pattern_eng, line):
+                for ele in re.findall('[a-zA-Z]+', line):
+                    eng_words_set.add(ele)
+
+        global all_eng_count
+        if len(eng_words_set) > 5:
+            all_eng_count += 1
+            print eng_words_set
         return ret_lines
 
 
@@ -45,3 +59,4 @@ def get_sorted_lines():
 
 
 get_sorted_lines()
+print all_eng_count
