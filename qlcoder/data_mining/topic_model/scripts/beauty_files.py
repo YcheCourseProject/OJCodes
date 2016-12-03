5514243db07eb3cea9691c5fa53dbf0058416379
cheyulin <yche@cse.ust.hk>
2016-12-03 19:32:19 +0800

    correctly use jieba tokenize words

--- qlcoder/data_mining/topic_model/scripts/beauty_files.py
+++ qlcoder/data_mining/topic_model/scripts/beauty_files.py
@@ -18,9 +18,9 @@ def extract_link(lines):
     return links
 
 
-lines = yche_util.remove_duplicate(yche_util.get_lines_single_file('../8000/4324.txt'))
+lines = yche_util.remove_duplicate(yche_util.get_lines_single_file('../8000/74.txt'))
 map_res = map(lambda line: jieba.posseg.cut(line), lines)
 words = reduce(lambda left, right: list(left) + list(right), map_res)
-# words = filter(lambda word, tag: word not in yche_util.stop_words_set and not re.match(r'[0-9]+', word), words)
 for word, tag in words:
-    print word, tag
+    if word not in yche_util.stop_words_set and re.match(r'n|a.*|v.*', tag) and not re.match(r'nr|ns|nz', tag):
+        print word, tag
