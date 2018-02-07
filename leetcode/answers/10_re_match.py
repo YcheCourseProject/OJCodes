c2e3ef0a4b151aa0af6abb2749e59022ae8cfa0f
CHE Yulin <yche@cse.ust.hk>
2018-02-07 16:10:28 +0800

    finish re match using dfs, insteads of nfa to dfa

--- leetcode/answers/10_re_match.py
+++ leetcode/answers/10_re_match.py
@@ -1,27 +1,50 @@
 class Solution(object):
-    @staticmethod
-    def get_star_pos(pat_str):
-        ret = []
-        for idx, ele in enumerate(pat_str):
-            if ele == '*':
-                ret.append((idx - 1, idx + 1))
-        return ret
-
     def isMatch(self, s, p):
         """
         :type s: str
         :type p: str
         :rtype: bool
         """
-        print Solution.get_star_pos(p)
+
+        def is_match_detail(s_beg, s_len, p_beg, p_len):
+            # bottom condition
+            if s_len == 0 and p_len == 0:
+                return True
+            if s_len == 0:
+                if p_len % 2 != 0:
+                    return False
+                for i in xrange(1 + p_beg, p_len + p_beg, 2):
+                    if p[i] != '*':
+                        return False
+                return True
+            if s_len != 0 and p_len == 0:
+                return False
+
+            # 1st: expand for '*', one-hop expansion
+            if p_len >= 2:
+                if p[p_beg + 1] == '*':
+                    return is_match_detail(s_beg, s_len, p_beg + 2, p_len - 2) or \
+                           (is_match_detail(s_beg + 1, s_len - 1, p_beg, p_len)
+                           if p[p_beg] == '.' or p[p_beg] == s[s_beg] else False)
+
+            # 2nd: for others
+            if p[p_beg] == '.' or p[p_beg] == s[s_beg]:
+                return is_match_detail(s_beg + 1, s_len - 1, p_beg + 1, p_len - 1)
+
+            # 3rd: not match here
+            return False
+
+        return is_match_detail(0, len(s), 0, len(p))
 
 
 if __name__ == '__main__':
-    Solution().isMatch('aa', 'a')
-    Solution().isMatch("aa", "a")
-    Solution().isMatch("aa", "aa")
-    Solution().isMatch("aaa", "aa")
-    Solution().isMatch("aa", "a*")
-    Solution().isMatch("aa", ".*")
-    Solution().isMatch("ab", ".*")
-    Solution().isMatch("aab", "c*a*b")
+    print Solution().isMatch('aa', 'a')
+    print Solution().isMatch("aa", "a")
+    print Solution().isMatch("aa", "aa")
+    print Solution().isMatch("aaa", "aa")
+    print Solution().isMatch("aa", "a*")
+    print Solution().isMatch("aa", ".*")
+    print Solution().isMatch("ab", ".*")
+    print Solution().isMatch("aab", "c*a*b")
+
+    print Solution().isMatch("a", ".*..a*")
